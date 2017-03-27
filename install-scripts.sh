1cca52b524b03bdcde71746e367415b4ef762f8e
Paul Baker <paul.baker@wgu.edu>
2017-03-27 12:22:39 -0600

    I set the symlinks incorrectly. They weren't actually pointing to the file but to themselves in the same directory.

--- install-scripts.sh
+++ install-scripts.sh
@@ -7,7 +7,7 @@ for i in "${SCRIPTS[@]}"; do
 		echo "${i} already symlinked in /usr/local/bin/"
 	else
 		echo "Symlinking ${i} to /usr/local/bin/${i}"
-		ln -s "./${i}" "/usr/local/bin/${i}"
+		ln -s "$(pwd)/${i}" "/usr/local/bin/${i}"
 		chmod +x "/usr/local/bin/${i}"
 	fi
 done
