079fa4cff9fc9415d08d3506279a91e71f3b62f2
Paul Baker <paul.nelson.baker@gmail.com>
2017-03-29 20:51:17 -0600

    Script to install npm packages.

---
+++ single-use-scripts/install-npm-packages.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+function install_if_missing
+{
+  if npm list -g "${1}" @>/dev/null; then
+    echo "${1} already installed"
+  else
+    echo ""
+    echo "Installing ${1}"
+    npm install -g "${1}"
+  fi
+}
+
+echo ""
+echo "Checking if NPM packages are installed is a costly"
+echo "opperation, so this script is slow because of NPM"
+echo ""
+
+PACKAGES=( '@angular/cli' 'splash-cli' 'ungit' )
+for i in "${PACKAGES[@]}"; do
+  install_if_missing "${i}"
+done
