67ae280ae8d627082223dee3b0196ee19ea17aec
Paul Baker <paul.nelson.baker@gmail.com>
2017-04-28 06:05:39 -0600

    Switching cask and regular installs.

--- single-use-scripts/install-homebrew.sh
+++ single-use-scripts/install-homebrew.sh
@@ -12,8 +12,8 @@ PACKAGES=( 'bash-completion' 'brew-cask-completion' 'm-cli' 'wget' 'coreutils' \
             'chromedriver' 'boost' 'boost-python' 'exercism' 'gradle' 'imagemagick' \
             'graphviz' 'git' 'git-secrets' 'maven' 'clang-format' 'node' 'tree' 'cmake' \
             'kotlin' 'qemu' 'libvirt' )
-CASK_PACKAGES=( 'atom' 'docker' 'kitematic' 'vagrant' 'virtualbox' 'firefox' )
-
+CASK_PACKAGES=( 'atom' 'docker' 'kitematic' 'vagrant' 'virtualbox' 'firefox' \
+                'xquartz' 'java' )
 function enterkey
 {
   echo ""
@@ -22,14 +22,8 @@ function enterkey
   read -r -p "Press enter key to continue..."
 }
 
-for i in "${PACKAGES[@]}"; do
-  if brew ls --versions "${i}" > /dev/null; then
-    echo "${i} is already installed"
-  else
-    brew install "${i}"
-    enterkey
-  fi
-done
+brew tap caskroom/cask
+brew tap homebrew/science
 for i in "${CASK_PACKAGES[@]}"; do
   if brew cask ls --versions "${i}" > /dev/null; then
     echo "${i} is already installed"
@@ -38,5 +32,14 @@ for i in "${CASK_PACKAGES[@]}"; do
     enterkey
   fi
 done
+for i in "${PACKAGES[@]}"; do
+  if brew ls --versions "${i}" > /dev/null; then
+    echo "${i} is already installed"
+  else
+    brew install "${i}"
+    enterkey
+  fi
+done
+
 echo ""
 echo "This has completed installing homebrew and packages!"
