927efe83e696954a99f2b97875e401531b041e09
Paul Baker <paul.nelson.baker@gmail.com>
2017-05-01 21:16:37 -0600

    Updating scripts.

--- global-scripts/install-scripts.sh
+++ global-scripts/install-scripts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-SCRIPTS=( 'git-ignore-java' 'update-stuff' 'watch-poms' 'cls' 'mvn-get-sources' )
+SCRIPTS=( 'git-ignore-java' 'update-stuff' 'watch-poms' 'cls' 'mvn-get-sources' 'mvn-get-deps' )
 for i in "${SCRIPTS[@]}"; do
 	if [ -L "/usr/local/bin/${i}" ]
 	then
