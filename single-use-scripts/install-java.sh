e161da742d4beb0987f5e12d6df9aac4636c11a5
Paul Baker <paul.nelson.baker@gmail.com>
2017-09-25 08:31:00 -0600

    Improved java install script
    No need for a dirty hack if there's a way to get it to play nice out of the box. Especially if it works for both maven and gradle off the bat. Makes life much easier.

--- single-use-scripts/install-java.sh
+++ single-use-scripts/install-java.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 set -e
-
 # Uninstall Java 9 off the bat, so we can fix our local installation
 if brew cask ls --versions "java" &>/dev/null; then
   echo "Uninstalling Java"
   brew cask uninstall java
 fi
-
+# Install jenv, java8 and java9
 brew install jenv
 brew cask install caskroom/versions/java8
 brew cask install java
-
 # Remove orphans or bad previous installs so we manage things from a clean state
 old_versions="$(jenv versions --bare)"
-echo "${old_versions}"
 for java_version in ${old_versions}; do
   if [ "${java_version}" == "system" ]; then
     continue
@@ -21,16 +18,15 @@ for java_version in ${old_versions}; do
   echo "Un-managing: ${java_version}"
   jenv remove "${java_version}"
 done
-
+# Manage all currently installed versions
 for path in /Library/Java/JavaVirtualMachines/*; do
   full_path="${path}/Contents/Home"
   echo "Now managing: ${full_path}"
   jenv add "${full_path}"
 done
-
+# Set default to Java 8
 jenv global 1.8
 jenv rehash
-
-# For whatever reason, maven doesn't seem to want to respect jenv.
-# Let's work around that.
-echo "JAVA_HOME=\$(/usr/libexec/java_home -v \$(jenv version-name))" > ~/.mavenrc
+# Get maven and gradle to play nice
+jenv enable-plugin maven
+jenv enable-plugin gradle
