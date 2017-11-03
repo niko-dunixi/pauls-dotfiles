cb0dc30ff05ab8d90c3c5d7c9df9b9c352a0a9be
Paul Baker <paulbaker@Pauls-MBP.hsd1.ut.comcast.net>
2017-11-03 03:07:59 -0600

    Fixing java installer

--- single-use-scripts/install-java.sh
+++ single-use-scripts/install-java.sh
@@ -2,31 +2,44 @@
 set -e
 # Uninstall Java 9 off the bat, so we can fix our local installation
 if brew cask ls --versions "java" &>/dev/null; then
-  echo "Uninstalling Java"
+  echo "Uninstalling Java 9"
   brew cask uninstall java
 fi
+if brew cask ls --versions "java8" &>/dev/null; then
+  echo "Uninstalling Java 8"
+  brew cask uninstall java8
+fi
+if brew ls --version "jenv" &>/dev/null; then
+  echo "Uninstalling jenv"
+  brew uninstall jenv
+fi
+(
+  if [ -d "/Library/Java/JavaVirtualMachines/" ]; then
+    cd "/Library/Java/JavaVirtualMachines/"
+    sudo rm -rf ./*
+  fi
+)
 # Install jenv, java8 and java9
 brew install jenv
+mkdir -p "${HOME}/.jenv/versions/"
 brew cask install caskroom/versions/java8
 brew cask install java
 # Remove orphans or bad previous installs so we manage things from a clean state
 old_versions="$(jenv versions --bare)"
 for java_version in ${old_versions}; do
-  if [ "${java_version}" == "system" ]; then
-    continue
-  fi
-  echo "Un-managing: ${java_version}"
   jenv remove "${java_version}"
 done
 # Manage all currently installed versions
 for path in /Library/Java/JavaVirtualMachines/*; do
   full_path="${path}/Contents/Home"
-  echo "Now managing: ${full_path}"
   jenv add "${full_path}"
 done
+eval "$(jenv init -)"
 # Set default to Java 8
 jenv global 1.8
 jenv rehash
 # Get maven and gradle to play nice (you might have to run these two commands manually)
 jenv enable-plugin maven
 jenv enable-plugin gradle
+echo ""
+jenv init
