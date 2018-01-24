897ae935e542ec079c099617c1bd9eb01e1a9096
Paul Baker <paul.nelson.baker@gmail.com>
2018-01-24 16:19:35 -0700

    Utilizing Brewfile feature
    This is a MUCH easier way to handle installed applications by homebrew. MUCH MUCH easier

--- setup-scripts/install-homebrew.sh
+++ setup-scripts/install-homebrew.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
+# Install OSX xcode utils
+echo "Will install xcode-select cli utilities..."
+sudo xcode-select --install || echo "... moving on"
 # Install homebrew, then we'll install packages
 if hash brew 2>/dev/null; then
   echo "Homebrew already installed, moving on to packages."
@@ -8,43 +11,5 @@ else
   echo "Homebrew installed, moving on to packages."
 fi
 brew update && brew upgrade
-PACKAGES=(
-  'bash-completion' 'brew-cask-completion' 'm-cli' 'wget' 'coreutils' \
-  'chromedriver' 'boost' 'boost-python' 'exercism' 'gradle' 'imagemagick' \
-  'graphviz' 'git' 'hg' 'git-secrets' 'maven' 'clang-format' 'node' 'tree' 'cmake' \
-  'kotlin' 'qemu' 'libvirt' 'mas' 'unrar' 'python' 'python3' 'mitmproxy' \
-  'wine' 'winetricks' 'zsh' 'shellcheck' 'jq' 'awscli' )
-CASK_PACKAGES=(
-  'atom' 'docker' 'kitematic' 'vagrant' 'virtualbox' 'firefox' \
-    'xquartz' 'android-sdk' 'android-ndk' 'google-chrome' \
-    # 'haskell-platform' \
-		'qlvideo' 'jetbrains-toolbox' )
-function enterkey
-{
-  echo ""
-  echo "Holding script execution, please follow caveat"
-  echo "instructions (if any) and then resume."
-  read -r -p "Press enter key to continue..."
-}
-
-brew tap caskroom/cask
-brew tap homebrew/science
-for i in "${CASK_PACKAGES[@]}"; do
-  if brew cask ls --versions "${i}" &>/dev/null; then
-    echo "${i} is already installed"
-  else
-    brew cask install "${i}"
-    enterkey
-  fi
-done
-for i in "${PACKAGES[@]}"; do
-  if brew ls --versions "${i}" &>/dev/null; then
-    echo "${i} is already installed"
-  else
-    brew install "${i}"
-    enterkey
-  fi
-done
-
-echo ""
-echo "This has completed installing homebrew and packages!"
+echo "Installing from Brewfile:"
+brew bundle
