3f4ba76cc7ee1a7207c930b790103c19946e233d
Paul Baker <paul.nelson.baker@gmail.com>
2018-01-25 02:21:04 -0700

    Asking for sudo and keeping it alive while the script is running

--- setup-scripts/install-all.sh
+++ setup-scripts/install-all.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+echo "I need sudo to install some stuff and manipulate the shell. Gimmie gimmie gimmie!"
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 current_directory="$(pwd)"
 root_directory="${current_directory%setup-scripts}"
 setup_scripts_directory="${root_directory}/setup-scripts/"
--- setup-scripts/install-oh-my-zsh.sh
+++ setup-scripts/install-oh-my-zsh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) "${USER}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sed -i '' 's/ZSH_THEME=.*/ZSH_THEME="af-magic"/' ~/.zshrc
