c8ffd96f25d40150743b0fe54836346fb34ed434
Paul Baker <paul.baker@wgu.edu>
2018-01-25 16:44:32 -0700

    Better initialization of my exercism projects.
    Conditional checking, recursive updating where necessary. I haven't touched them in a while so this is a good opertunity to clean them up.

---
+++ setup-scripts/setup-exercism-exercises.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo "NOTE: Install git, exercism, and any submodule languages if you haven't already!"
+echo "      See install-homebrew.sh and Brewfile to do this easily on OSX"
+
+# Clone the project, if needed
+exio_directory="${HOME}/exercism"
+printf "\nUsing directory: ${exio_directory}\n"
+if [ ! -d "${exio_directory}" ]; then
+  git clone --recursive git@github.com:paul-nelson-baker/exercism-solutions.git "${exio_directory}"
+elif [ ! -d "${exio_directory}/.git" ]; then
+  echo "The exercism directory exists, but is NOT git controlled. Failing script because something is wrong."
+  exit 1
+else
+  echo "Already cloned, proceeding..."
+fi
+
+# Update the submodules
+(
+cd "${exio_directory}"
+./update-submodules.sh
+)
+
+# Done message
+echo "Project has been downloaded and submodules have been recursively updated."
