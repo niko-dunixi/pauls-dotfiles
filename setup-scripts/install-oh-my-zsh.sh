f3cb8543a972277baa7db82633ad2121c5d9a437
Paul Baker <paul.nelson.baker@gmail.com>
2018-01-25 03:50:48 -0700

    Better option enabling/manipulation

--- setup-scripts/install-oh-my-zsh.sh
+++ setup-scripts/install-oh-my-zsh.sh
@@ -4,13 +4,30 @@ set -e
 sudo chsh -s $(which zsh) "${USER}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sed -i '' 's/ZSH_THEME=.*/ZSH_THEME="af-magic"/' ~/.zshrc
-sed -i '' 's/plugins=.*/plugins=(git node mvn gradle ng npm python web-search yarn brew docker jira aws)/' ~/.zshrc
+zshrc_file="${HOME}/.zshrc"
+echo "Using .zshrc file: ${zshrc_file}"
+
+cp "${zshrc_file}" "${zshrc_file}.bak"
+echo "Setting default theme to 'af-magic'"
+perl -0777pe 's/ZSH_THEME=".*?"/ZSH_THEME="af-magic"/s' "${zshrc_file}.bak" > "${zshrc_file}"
+cp "${zshrc_file}" "${zshrc_file}.bak"
+echo "Enabling plugins"
+perl -0777pe 's/plugins=\(.*?\)/plugins=(git node mvn gradle ng npm python web-search yarn brew docker jira aws)/sg' "${zshrc_file}.bak" > "${zshrc_file}"
 
 current_directory=$(pwd)
 root_directory="${current_directory%setup-scripts}"
 zsh_custom_directory="${root_directory}/zsh-custom/"
 
-if ! grep -Fxq "source ${zsh_custom_directory}/my-aliases-exports-etc.zsh" ~/.zshrc; then
-  echo "source ${zsh_custom_directory}/my-aliases-exports-etc.zsh" >> ~/.zshrc
+# cp "${zshrc_file}" "${zshrc_file}.bak"
+# echo "Enabling custom directory"
+# perl -0777pe "s/\\# ZSH_CUSTOM=.*?\b/ZSH_CUSTOM=\"${zsh_custom_directory}\"/s" "${zshrc_file}.bak" > "${zshrc_file}"
+# cp "${zshrc_file}" "${zshrc_file}.bak"
+# perl -0777pe "s/ZSH_CUSTOM=.*?\b/ZSH_CUSTOM=\"${zsh_custom_directory}\"/s" "${zshrc_file}.bak" > "${zshrc_file}"
+# exit 1
+
+rm "${zshrc_file}.bak"
+
+echo "Sourcing ${zsh_custom_directory}/my-aliases-exports-etc.zsh to the end of the .zshrc file"
+if ! grep -Fxq "source ${zsh_custom_directory}/my-aliases-exports-etc.zsh" "${zshrc_file}"; then
+  echo "source ${zsh_custom_directory}/my-aliases-exports-etc.zsh" >> "${zshrc_file}"
 fi
