28ef44c3dfaa6817ea11fd4d0a9ecffe22243c31
Paul Baker <paul.nelson.baker@gmail.com>
2018-03-22 03:36:35 -0600

    Renaming file to be sensical

--- setup-scripts/install-all.sh
+++ setup-scripts/install-all.sh
@@ -5,21 +5,22 @@ echo "I need sudo to install some stuff and manipulate the shell. Gimmie gimmie 
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Figure out what directory we're actually executing from
 current_directory="$(pwd)"
 root_directory="${current_directory%setup-scripts}"
 setup_scripts_directory="${root_directory}/setup-scripts/"
 cd "${setup_scripts_directory}"
-
+# OSX or Linux?
 my_os="$(uname)"
-
 if [ "${my_os}" == "darwin" ]; then
   ./install-homebrew-packages.sh
 else
   ./install-apt-packages.sh
 fi
-./setup-java.sh
+./setup-java-jenv.sh
 ./install-npm-packages.sh
 ./install-shellscripts.sh
 ./install-oh-my-zsh.sh
+./setup-oh-my-zsh.sh
 ./setup-git-commit-message.sh
 ./setup-ssh-git.sh
