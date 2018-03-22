fa9f631cf9385c60b7a6a7a00717058a2934f4f2
Paul Baker <paul.nelson.baker@gmail.com>
2018-03-22 02:16:53 -0600

    Got this working more reliably by using python embedded script instead of perl replacemnts.

--- setup-scripts/install-oh-my-zsh.sh
+++ setup-scripts/install-oh-my-zsh.sh
@@ -8,17 +8,6 @@ sudo chsh -s $(which zsh) "${USER}"
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Liquidsoul/oh-my-zsh/master/tools/install.sh) --silent || true"
 
-#zshrc_file="${HOME}/.zshrc"
-#printf "\nUsing .zshrc file: ${zshrc_file}\n"
-
-#cp "${zshrc_file}" "${zshrc_file}.bak"
-#echo "Setting default theme to 'af-magic'"
-#perl -0777pe 's/ZSH_THEME=".*?"/ZSH_THEME="af-magic"/s' "${zshrc_file}.bak" > "${zshrc_file}"
-#cp "${zshrc_file}" "${zshrc_file}.bak"
-#echo "Enabling plugins"
-#perl -0777pe 's/plugins=\(.*?\)/plugins=(git node mvn gradle ng npm python web-search yarn brew docker jira aws)/sg' "${zshrc_file}.bak" > "${zshrc_file}"
-#
-
 # Link our custom stuff
 current_directory=$(pwd)
 root_directory="${current_directory%setup-scripts}"
@@ -32,17 +21,28 @@ for source in ${custom_from_install_directory}/*.zsh; do
   ln -s "${source}" "${destination}"
 done
 
+# Make changes to the zshrc file so we have our theme and plugins set
+export ohmyzsh_rc
+python3 - << END
+import os
+import re
 
+# Load the zshrc file
+ohmyzsh_rc = os.getenv('ohmyzsh_rc')
+print('Loading', ohmyzsh_rc)
+with open(ohmyzsh_rc, 'r') as file:
+    file_data = file.read()
 
-# cp "${zshrc_file}" "${zshrc_file}.bak"
-# echo "Enabling custom directory"
-# perl -0777pe "s/\\# ZSH_CUSTOM=.*?\b/ZSH_CUSTOM=\"${zsh_custom_directory}\"/s" "${zshrc_file}.bak" > "${zshrc_file}"
-# cp "${zshrc_file}" "${zshrc_file}.bak"
-# perl -0777pe "s/ZSH_CUSTOM=.*?\b/ZSH_CUSTOM=\"${zsh_custom_directory}\"/s" "${zshrc_file}.bak" > "${zshrc_file}"
-# exit 1
+print('Setting theme')
+file_data = re.sub(r"^ZSH_THEME=\".*?\"", 'ZSH_THEME="af-magic"', file_data, flags=re.MULTILINE)
+# Set the plugins we want
+print('Setting plugins')
+file_data = re.sub(r"^plugins=\(.*?\)",
+                  'plugins=(git node mvn gradle ng npm python web-search yarn brew docker jira aws)',
+                   file_data, flags=re.MULTILINE | re.DOTALL)
 
-#rm "${zshrc_file}.bak"
-#echo "Sourcing ${zsh_custom_directory}/my-aliases-exports-etc.zsh to the end of the .zshrc file"
-#if ! grep -Fxq "source ${zsh_custom_directory}/my-aliases-exports-etc.zsh" "${zshrc_file}"; then
-#  echo "source ${zsh_custom_directory}/my-aliases-exports-etc.zsh" >> "${zshrc_file}"
-#fi
+print('Setting contents back for', ohmyzsh_rc)
+# Save the contents back to the zshrc file
+with open(ohmyzsh_rc, 'w') as file:
+    file.write(file_data)
+END
