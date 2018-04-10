ab3588f82e3b87a9150e0aa1a34d9a025e0008a1
Paul Baker <paul.baker@wgu.edu>
2018-04-10 16:17:58 -0600

    More cleaning of docker images.

--- zsh-custom/functions-docker.zsh
+++ zsh-custom/functions-docker.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 function docker-clean()
 {
+  for cnt in $(docker ps -a -q); do
+    docker rm -f "${cnt}"
+  done
+  for img in $(docker images -q); do
+    docker rmi -f "${img}"
+  done
   docker system prune -f
 }
