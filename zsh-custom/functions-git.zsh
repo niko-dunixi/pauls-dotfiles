e07efd420f2efbe50da018c05f396ff391b0002c
Paul Baker <paul.baker@wgu.edu>
2018-04-17 13:26:41 -0600

    Adding functions for quick-setting the git author of a repo.

--- zsh-custom/functions-git.zsh
+++ zsh-custom/functions-git.zsh
@@ -3,3 +3,15 @@ function git-current-branch()
 {
   git branch | grep '*' | awk '{print $2}'
 }
+
+function git-set-personal-author()
+{
+  git config user.name "Paul N. Baker"
+  git config user.email "paul.nelson.baker@gmail.com"
+}
+
+function git-set-professional-author()
+{
+  git config user.name "Paul Baker"
+  git config user.email "paul.baker@wgu.edu"
+}
