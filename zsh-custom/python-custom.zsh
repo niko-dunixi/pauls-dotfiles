638b762f6df46ccf2c58e659ca16a551af5a50f1
Paul Baker <paul.nelson.baker@gmail.com>
2018-05-20 10:29:03 -0600

    Conditional anaconda sourcing. We can now add/remove anaconda as much as we want and expect things to work as they should.

--- zsh-custom/python-custom.zsh
+++ zsh-custom/python-custom.zsh
@@ -1,9 +1,18 @@
 #!/usr/bin/env zsh
-# I don't like using anaconda, but I'm keepting these for reference.
 
-# This is the old way
-# export PATH=/usr/local/anaconda3/bin:"${PATH}"
+# TODO: extrapolate this and place it as some kind of "import"?
+function iscommand
+{
+  hash "${1}" 2>/dev/null
+}
 
-# This is the new way
-#. "/usr/local/anaconda3/etc/profile.d/conda.sh"
-#conda activate
+# I don't like using anaconda (anymore), but if I am using it
+# I should use it and source the profiles correctly
+if iscommand conda; then
+  # This is the old way
+  # export PATH=/usr/local/anaconda3/bin:"${PATH}"
+
+  # This is the new way
+  . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+  conda activate
+fi
