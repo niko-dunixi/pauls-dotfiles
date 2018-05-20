9a20223008a5c4eff243693631d4647c643fd69c
Paul Baker <paul.nelson.baker@gmail.com>
2018-05-20 10:22:59 -0600

    Disabling anaconda. I'm not using it anymore.

--- zsh-custom/python-custom.zsh
+++ zsh-custom/python-custom.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
+# I don't like using anaconda, but I'm keepting these for reference.
+
+# This is the old way
 # export PATH=/usr/local/anaconda3/bin:"${PATH}"
-. "/usr/local/anaconda3/etc/profile.d/conda.sh"
-conda activate
+
+# This is the new way
+#. "/usr/local/anaconda3/etc/profile.d/conda.sh"
+#conda activate
