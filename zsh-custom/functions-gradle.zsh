e8d44761f6554755e6f9637df6436d64525999b1
Paul Baker <paul.nelson.baker@gmail.com>
2018-06-18 10:53:53 -0600

    Script to convert maven to gradle

---
+++ zsh-custom/functions-gradle.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+# function create-java-fx()
+# {
+#     capture_file="$(mktemp)"
+#     script -q "${capture_file}" "mvn archetype:generate -DarchetypeGroupId=com.zenjava -DarchetypeArtifactId=javafx-basic-archetype"
+#     cat "${capture_file}"
+# }
+
+function gradle-from-maven()
+{
+  gradle init --type pom
+  if [ -f "pom.xml" ]; then
+    mv "pom.xml" ".pom.xml.$(date +%s).backup"
+  fi
+}
