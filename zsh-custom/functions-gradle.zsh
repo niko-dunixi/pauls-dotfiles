e099234fa4b490de01f8c4489248c01888c369d1
Paul Baker <paul.baker@wgu.edu>
2018-06-25 06:50:42 -0600

    Kotlin native method

--- zsh-custom/functions-gradle.zsh
+++ zsh-custom/functions-gradle.zsh
@@ -20,3 +20,35 @@ function create-java-folder-struct()
 {
   mkdir -p ./src/{main,test}/java
 }
+
+function gradle-init-kotlin-native()
+{
+  [ ! -z "${1}" ] || fail
+  mkdir -p "${1}/src/main/kotlin"
+  (
+    echo "buildscript {"
+    echo "  repositories {"
+    echo "    mavenCentral()"
+    echo "    maven {"
+    echo "      url \"https://dl.bintray.com/jetbrains/kotlin-native-dependencies\""
+    echo "    }"
+    echo "  }"
+    echo "  dependencies {"
+    echo "    classpath \"org.jetbrains.kotlin:kotlin-native-gradle-plugin:+\""
+    echo "  }"
+    echo "}"
+    echo ""
+    echo "apply plugin: 'konan'"
+    echo ""
+    echo "konan.targets = ['macbook', 'linux', 'mingw']"
+    echo ""
+    echo "konanArtifacts {"
+    echo "  program('hello')"
+    echo "}"
+  ) > "${1}/build.gradle"
+  (
+    echo "fun main(args: Array<String>) {"
+    echo "  println(\"Hello Gradle!\")"
+    echo "}"
+  ) > "${1}/src/main/kotlin/hello.kt"
+}
