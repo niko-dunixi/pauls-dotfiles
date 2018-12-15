4268a31998b61053ea2f53e1f48200a7d02354ae
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-15 16:47:32 -0700

    Making more os agnostic

--- setup-scripts/install-apt-packages.sh
+++ setup-scripts/install-apt-packages.sh
@@ -8,11 +8,9 @@ install()
   sudo apt install -y "${@}"
 }
 
-install xclip
-install git
-install vim
+install vim curl gcc g++ make git xclip
+install zsh terminator
 install openjdk-8-jre maven gradle
-install curl
 # Kotlin
 (
 set -e
@@ -20,7 +18,22 @@ curl -s https://get.sdkman.io | bash
 source "${HOME}/.sdkman/bin/sdkman-init.sh"
 sdk install kotlin
 )
-install zsh terminator
+
+# NodeJS v 10 is the latest LTS, as of 12/15/2018
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt update --fix-missing
+install nodejs
+if [ -f /usr/bin/nodejs ] && [ ! -f /usr/bin/node ]; then
+  echo "Symlinking: ln -s /usr/bin/nodejs /usr/bin/node"
+  sudo -H ln -s /usr/bin/nodejs /usr/bin/node
+elif [ -L /usr/bin/node ]; then
+  echo "Symlink already exists for: /usr/bin/node"
+fi
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update --fix-missing
+install yarn
+
 # jenv
 (
 git clone https://github.com/gcuisinier/jenv.git "${HOME}/.jenv" || git --git-dir "${HOME}/.jenv/.git" pull
