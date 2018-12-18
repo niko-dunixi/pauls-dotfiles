71b1c442ee35e97fe5bb716165a89572014e5e85
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-18 14:39:31 -0700

    Enabling anti-aliasing for text

--- setup-scripts/setup-osx-defaults.sh
+++ setup-scripts/setup-osx-defaults.sh
@@ -5,3 +5,7 @@ set -e
 # https://apple.stackexchange.com/a/344186/124061
 defaults write com.apple.iTunesHelper ignore-devices 1
 
+# Re-Enable anti-aliasing for text
+# https://ahmadawais.com/fix-macos-mojave-font-rendering-issue/
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool FALSE
+
