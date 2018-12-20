a1a4b1ca97098b8f7e13cc70242181f99e672246
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-19 17:57:06 -0700

    Fixing crap

--- setup-scripts/install-npm-packages.sh
+++ setup-scripts/install-npm-packages.sh
@@ -8,19 +8,11 @@ function install_if_missing
   else
     echo ""
     echo "Installing ${1}"
-    command="sudo -H npm install -g ${1}"
+    command="npm install -g ${1}"
     echo "Running: ${command}"
     $command || echo "Failed to run: ${command}"
   fi
 }
-
-echo "Updating NPM..."
-sudo -H npm install -g npm@next || echo "Couldn't update NPM to npm@next"
-
-echo ""
-echo "Checking if NPM packages are installed is a costly"
-echo "opperation, so this script is slow because of NPM"
-echo ""
 
 PACKAGES=( 'npx' )
 for i in "${PACKAGES[@]}"; do
