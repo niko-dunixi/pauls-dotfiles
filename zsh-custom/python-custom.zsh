555d2635d440d0c4ead3e5504ab5c8ec2758c8fe
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-12-31 12:16:58 -0700

    Adding pip installed packages to PATH

--- zsh-custom/python-custom.zsh
+++ zsh-custom/python-custom.zsh
@@ -6,6 +6,11 @@ function iscommand
   hash "${1}" 2>/dev/null
 }
 
+for py_version in $(ls "${HOME}/Library/Python/"); do
+  echo "${py_version}"
+  export PATH="${PATH}:${HOME}/Library/Python/${py_version}/bin"
+done
+
 # I don't like using anaconda (anymore), but if I am using it
 # I should use it and source the profiles correctly
 if iscommand conda; then
