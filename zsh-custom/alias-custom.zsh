264107b16a716ab721b31fa92099044fafd84bb7
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-01-17 11:50:13 -0700

    function-ifying the check port and adding freeing ports which call the check-port

--- zsh-custom/alias-custom.zsh
+++ zsh-custom/alias-custom.zsh
@@ -20,3 +20,40 @@ function interobang-paul()
 {
   echo "$(interobang)aul Baker!"
 }
+
+function cycle-magnet()
+{
+  if [ -d /Applications/Magnet.app ]; then
+    pkill -f Magnet
+    open -a Magnet
+  fi
+}
+
+function check-port()
+{
+  for port in "${@}"; do
+    lsof -nP -i4TCP:"${port}" | grep LISTEN
+  done
+}
+
+function free-port()
+{
+  for port in "${@}"; do
+    check-port "${port}" | awk '{ print $2 }' | xargs kill
+  done
+}
+
+function free-port-8080()
+{
+  free-port 8080
+}
+#function airport()
+#{
+#  ap=$(find /System -type f -name "airport" -print 2>/dev/null | head -n 1)
+#  $(sudo ap "${@}")
+#}
+#
+#function find-my-friends()
+#{
+#  airport en1 sniff 1
+#}
