765784a6e5a9f9ad8318bdc01b3607949cf22f8f
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-22 11:52:08 -0700

    Moving braces.

--- zsh-custom/aws-custom.zsh
+++ zsh-custom/aws-custom.zsh
@@ -2,22 +2,18 @@
 #alias aws-paul="aws --profile paulbaker"
 #alias aws-dev="aws --profile wgu-dev"
 #alias aws-sbx="aws --profile sbx"
-function aws-sbx()
-{
+function aws-sbx() {
   aws --profile sbx "${@}"
 }
 
-function aws-paul()
-{
+function aws-paul() {
   aws --profile paulbaker "${@}"
 }
 
-function aws-token-refresh()
-{
+function aws-token-refresh() {
   PYTHONWARNINGS="ignore" aws-adfs login --profile=master --adfs-host=adfs.wgu.edu --no-ssl-verification
 }
 
-function validate-template()
-{
+function validate-template() {
   aws-paul cloudformation validate-template --template-body "file://${@}"
 }
